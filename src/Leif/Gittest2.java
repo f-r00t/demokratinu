30cb4f23bbb50093284af74c80dadfe18327b85a
HalfLeif <edarathaan@hotmail.com>
2013-03-23 15:44:07 +0100

    Testar gitbash

--- src/Leif/Gittest2.java
+++ src/Leif/Gittest2.java
@@ -7,7 +7,8 @@ public class Gittest2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Leif was here");
+		System.out.println("Leif was here... again");
+		//
 	}
 
 }
