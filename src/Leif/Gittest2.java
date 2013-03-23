8623098977a822fc692ebfdcdfa190a7cc442f2e
HalfLeif <edarathaan@hotmail.com>
2013-03-23 15:30:07 +0100

    Testar gitbash igen...

---
+++ src/Leif/Gittest2.java
@@ -0,0 +1,13 @@
+package Leif;
+
+public class Gittest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Leif was here");
+	}
+
+}
