ccd93d78c42f844cd8d9d344a14c3584c0cae50b
HalfLeif <edarathaan@hotmail.com>
2013-04-16 20:24:00 +0200

    Continued work on the ArticleRetriever

--- src/com/example/pocketpolitics/control/ArticleRetriever.java
+++ src/com/example/pocketpolitics/control/ArticleRetriever.java
@@ -1,9 +1,13 @@
 package com.example.pocketpolitics.control;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 
 
 
@@ -15,13 +19,27 @@ public class ArticleRetriever {
 	private static SyndFeed feed;
 	
 	private ArticleRetriever(){
-		URL feedSource;
 		try {
-			feedSource = new URL(FEED_URL);
+			
+			URL feedSource = new URL(FEED_URL);
+			SyndFeedInput input = new SyndFeedInput();
+			if(feedSource!=null)
+				feed = input.build(new XmlReader(feedSource));
+			
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FeedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 		
 	}
 	public static ArticleRetriever getInstance(){
