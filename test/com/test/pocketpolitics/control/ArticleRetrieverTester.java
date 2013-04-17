88671ba8e94666e03908bd977728d951adc69a91
HalfLeif <edarathaan@hotmail.com>
2013-04-17 16:21:24 +0200

    Android JUnit testing: it Works! :)

---
+++ test/com/test/pocketpolitics/control/ArticleRetrieverTester.java
@@ -0,0 +1,10 @@
+package com.test.pocketpolitics.control;
+
+import android.test.AndroidTestCase;
+
+public class ArticleRetrieverTester extends AndroidTestCase {
+	
+	public void testAnything() throws Throwable {
+		assertTrue(1+1==2);
+	}
+}
