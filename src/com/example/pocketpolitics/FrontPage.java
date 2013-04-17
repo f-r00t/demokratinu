c38353b4c80f36ff51313f6fe57e11639fdf88fc
Björn <theturnipman@gmail.com>
2013-04-18 01:45:49 +0200

    Changed FrontPage.java so it initiates the listView.

--- src/com/example/pocketpolitics/FrontPage.java
+++ src/com/example/pocketpolitics/FrontPage.java
@@ -4,20 +4,45 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.pocketpolitics.control.ArticleListAdapter;
+import com.example.pocketpolitics.model.Article;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
 public class FrontPage extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_front_page);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.front_page, menu);
-		return true;
-	}
-	
+      private ListView listViewArticles;
+      private Context ctx;
+     
+      @Override
+      public void onCreate(Bundle savedInstanceState) {
 
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.article_view);
+            ctx=this;
+            List articleList = new ArrayList();
+            
+            //Replace this with the factory later, really ugly code incoming:
+            Article a = new Article();
+            a.setTitle("Artikel A");
+            articleList.add(a);
+            Article b = new Article();
+            b.setTitle("Artikel B");
+            articleList.add(b);
+            Article c = new Article();
+            c.setTitle("Artikel C");
+            articleList.add(c);
+
+            listViewArticles =  ( ListView ) findViewById( R.id.article_list);
+            listViewArticles.setAdapter(new ArticleListAdapter(ctx, R.layout.article_view, articleList));
+      }
 }
