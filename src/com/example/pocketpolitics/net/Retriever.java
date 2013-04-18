cc6651b5177b60f174709c092aedd107227e72a5
HalfLeif <edarathaan@hotmail.com>
2013-04-18 15:28:19 +0200

    New class: TextRetriever

--- src/com/example/pocketpolitics/net/Retriever.java
+++ src/com/example/pocketpolitics/net/Retriever.java
@@ -16,6 +16,11 @@ public class Retriever {
 		return INSTANCE;
 	}
 	
+	/**
+	 * Retrieves the latest articles in the RSS "Beslut i korthet" from riksdagen.se
+	 * @param update True if want to update the RSS (access the server anew)
+	 * @return A list of titles for articles
+	 */
 	public List<String> getImportantArticleTitles(boolean update){
 		if(update)
 			artRet.update();
---
+++ src/com/example/pocketpolitics/net/TextRetriever.java
@@ -0,0 +1,6 @@
+package com.example.pocketpolitics.net;
+
+class TextRetriever {
+	
+	
+}
