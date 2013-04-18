66166dd3c8bc50241079ab637b48470baa2ba7cc
Johan Hasselqvist <u.j.hasselqvist@gmail.com>
2013-04-18 21:53:00 +0200

    Adding constructors and javadoc to most model classes

--- src/com/example/pocketpolitics/model/Party.java
+++ src/com/example/pocketpolitics/model/Party.java
@@ -12,15 +12,30 @@ public class Party {
 		this.logo = logo;
 	}
 	
+	/**
+	 * @return The logotype view of this party
+	 */
 	public ImageView getLogo() {
 		return logo;
 	}
+	
+	/**
+	 * @param logo The logotype view of this party
+	 */
 	public void setLogo(ImageView logo) {
 		this.logo = logo;
 	}
+	
+	/**
+	 * @return The name of this party
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * @param name The name of this party
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
