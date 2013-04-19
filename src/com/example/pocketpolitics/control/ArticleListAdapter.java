3d37db9b1b13d4fdf630952205531e98f62a57e5
Björn <theturnipman@gmail.com>
2013-04-19 15:46:29 +0200

    Changed the constructor and getView method.

--- src/com/example/pocketpolitics/control/ArticleListAdapter.java
+++ src/com/example/pocketpolitics/control/ArticleListAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.example.pocketpolitics.R;
 import com.example.pocketpolitics.model.Article;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
@@ -18,33 +19,45 @@ import android.widget.TextView;
 
 public class ArticleListAdapter extends BaseAdapter{
 
-      private int resource;
-      private LayoutInflater inflater;
-      private Context context;
+	LayoutInflater inflater;
+	List<Article> items;
 
-	public ArticleListAdapter ( Context ctx, int resourceId, List objects) {
-            super( );
-            resource = resourceId;
-            inflater = LayoutInflater.from( ctx );
-            context=ctx;
+      public ArticleListAdapter(Activity context, List<Article> items) {  
+		    super();
+			
+		    this.items = items;
+		    this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
       }
+      
+      @Override  
+      public View getView(final int position, View convertView, ViewGroup parent) {  
 
+      	View vi=convertView;
+          
+          if(convertView==null)
+              vi = inflater.inflate(R.layout.article_view, null);
+              
+          
+            
+          return vi;  
+      }
+      /*
       @Override
       public View getView ( int position, View convertView, ViewGroup parent ) {
 
-            /* create a new view of my layout and inflate it in the row */
+            //create a new view of my layout and inflate it in the row 
             convertView = ( LinearLayout ) inflater.inflate( resource, null );
 
-            /* Extract the article's object to show */
+            // Extract the article's object to show 
             Article article = (Article) getItem( position );
 
-            /* Take the TextView from layout and set the articel's title */
+            /* Take the TextView from layout and set the articel's title
             TextView txtName = (TextView) convertView.findViewById(R.id.articleTitle);
             txtName.setText(article.getTitle());
             
             return convertView;
-      }
-
+      	}
+       */
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
