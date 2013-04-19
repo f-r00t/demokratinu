2f4675989fefea1ea50bd3c04cc5a3de7f5a4d0e
Björn <theturnipman@gmail.com>
2013-04-19 15:46:58 +0200

    Auto-generated things.

--- gen/com/example/pocketpolitics/R.java
+++ gen/com/example/pocketpolitics/R.java
@@ -30,14 +30,15 @@ public final class R {
         public static final int vpk=0x7f020006;
     }
     public static final class id {
-        public static final int ImageButton13=0x7f080005;
-        public static final int ImageButton14=0x7f080003;
-        public static final int ImageButton15=0x7f080001;
-        public static final int TextView13=0x7f080002;
-        public static final int TextView14=0x7f080004;
-        public static final int action_settings=0x7f080007;
-        public static final int articleTitle=0x7f080006;
-        public static final int article_list=0x7f080000;
+        public static final int ImageButton13=0x7f080006;
+        public static final int ImageButton14=0x7f080004;
+        public static final int ImageButton15=0x7f080002;
+        public static final int TextView13=0x7f080003;
+        public static final int TextView14=0x7f080005;
+        public static final int action_settings=0x7f080008;
+        public static final int articleTitle=0x7f080007;
+        public static final int article_list=0x7f080001;
+        public static final int linearLayout1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_front_page=0x7f030000;
