2eb0f61b82806567a94ed6ae8cf665826ef16929
Björn <theturnipman@gmail.com>
2013-04-20 17:07:57 +0200

    Renamed TextView ids in article_list_item into TextViewLikes and TextViewDislikes to better represent what they are.

--- gen/com/example/pocketpolitics/R.java
+++ gen/com/example/pocketpolitics/R.java
@@ -33,8 +33,8 @@ public final class R {
         public static final int ImageButton13=0x7f080006;
         public static final int ImageButton14=0x7f080004;
         public static final int ImageButton15=0x7f080002;
-        public static final int TextView13=0x7f080003;
-        public static final int TextView14=0x7f080005;
+        public static final int TextViewDislikes=0x7f080005;
+        public static final int TextViewLikes=0x7f080003;
         public static final int action_settings=0x7f080008;
         public static final int articleTitle=0x7f080007;
         public static final int article_list=0x7f080001;
