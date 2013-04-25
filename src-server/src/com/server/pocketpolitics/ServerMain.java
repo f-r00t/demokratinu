375e6227a60c8f73f8564b933d13ef778d6541a6
HalfLeif <edarathaan@hotmail.com>
2013-04-25 14:38:46 +0200

    Server: connecting to local SQL

--- src-server/src/com/server/pocketpolitics/ServerMain.java
+++ src-server/src/com/server/pocketpolitics/ServerMain.java
@@ -1,14 +1,32 @@
 package com.server.pocketpolitics;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.server.pocketpolitics.sql.LocalConnect;
+
 public class ServerMain {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Leif was truly here...");
+		String query = "SELECT * FROM votes";
+		
+		try {
+			ResultSet r =  LocalConnect.run(query);
+			
+			while(r.next()){
+				System.out.println(r.getString("voter")+" "+r.getString("issue")+" "+r.getString("opinion"));
+			}
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error in ServerMain: Class not found");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Error in ServerMain: SQL Exception");
+			e.printStackTrace();
+		}
 	}
 
 }
--- src-server/src/com/server/pocketpolitics/sql/LocalConnect.java
+++ src-server/src/com/server/pocketpolitics/sql/LocalConnect.java
@@ -2,26 +2,33 @@ package com.server.pocketpolitics.sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 
 
 public class LocalConnect{
 
-	private static final String URL = "jdbc:mysql://localhost/";
+	private static final String URL = "jdbc:mysql://localhost/pocketpolitics_test";
 	private static final String uname = "xxx";
 	private static final String pwd = "###";
 	
-	public void getConnection() throws ClassNotFoundException, SQLException {
+	public static ResultSet run(String query) throws ClassNotFoundException, SQLException {
 		Connection con = null;
 		try{
 			Class.forName("com.mysql.jdb.Driver");
 			con = DriverManager.getConnection(URL, uname, pwd);
 			
-			//...
+			Statement statem = con.createStatement();
+			
+			
+			ResultSet res = statem.executeQuery(query);
+			
+			return res;
 		}
 		finally {
-			if (con!=null){
+			if (con!=null && !con.isClosed()){
+				System.out.println("Closing connection to SQL");
 				con.close();
 			}
 			
