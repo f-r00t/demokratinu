ebf6b48acba3858a01f29682531fcafaefa19133
HalfLeif <edarathaan@hotmail.com>
2013-04-25 14:26:20 +0200

    Rearranged server sql classes

--- src-server/src/com/server/pocketpolitics/sql/Connectable.java
+++
@@ -1,8 +0,0 @@
-package com.server.pocketpolitics.sql;
-
-import com.mysql.jdbc.Connection;
-
-public interface Connectable {
-
-	public Connection getConnection() throws InstantiationException, IllegalAccessException, ClassNotFoundException ;
-}
--- src-server/src/com/server/pocketpolitics/sql/LocalConnect.java
+++ src-server/src/com/server/pocketpolitics/sql/LocalConnect.java
@@ -1,16 +1,32 @@
 package com.server.pocketpolitics.sql;
 
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class LocalConnect extends Connect{
 
-	private static String URL = "jdbc:mysql://";
+
+public class LocalConnect{
+
+	private static final String URL = "jdbc:mysql://localhost/";
+	private static final String uname = "xxx";
+	private static final String pwd = "###";
 	
-	@Override
-	public Connection getConnection() {
-		// TODO Auto-generated method stub
+	public void getConnection() throws ClassNotFoundException, SQLException {
+		Connection con = null;
+		try{
+			Class.forName("com.mysql.jdb.Driver");
+			con = DriverManager.getConnection(URL, uname, pwd);
+			
+			//...
+		}
+		finally {
+			if (con!=null){
+				con.close();
+			}
+			
+		}
 		
-		return null;
 	}
 
 	
