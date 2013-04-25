786d0994c2a85293d4fc3c5cbe12829d1fc85bba
HalfLeif <edarathaan@hotmail.com>
2013-04-25 15:38:29 +0200

    Added folder Sensitive

--- src-server/src/com/server/pocketpolitics/sql/LocalConnect.java
+++ src-server/src/com/server/pocketpolitics/sql/LocalConnect.java
@@ -1,8 +1,11 @@
 package com.server.pocketpolitics.sql;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,13 +16,16 @@ import java.sql.Statement;
 public class LocalConnect{
 
 	private static final String URL = "jdbc:mysql://localhost/pocketpolitics_test";
+	private static final String path = "../sensitive/sensitive_sql.txt";
 	
 	public static void runTest() throws ClassNotFoundException, SQLException {
 		String query = "SELECT * FROM votes";
 		Connection con = null;
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection(URL, read("Username: "), read("Password: "));
+			String[] sens = readFromFile(path);
+			//con = DriverManager.getConnection(URL, read("Username: "), read("Password: "));
+			con = DriverManager.getConnection(URL, sens[0], sens[1]);
 			
 			Statement statem = con.createStatement();
 			
@@ -43,7 +49,7 @@ public class LocalConnect{
 		
 	}
 	
-	private static String read(String ask){
+	private static String readFromConsole(String ask){
 		System.out.print(ask);
 		try{
 			BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
@@ -56,6 +62,30 @@ public class LocalConnect{
 			e.printStackTrace();
 			return "";
 		}
+	}
+	
+	private static String[] readFromFile(String path){
+		try {
+			String[] str = new String[2];
+			
+			FileInputStream fs = new FileInputStream(path);
+			BufferedReader bfr = new BufferedReader(new InputStreamReader(fs, "UTF-8"));
+			str[0] = bfr.readLine();
+			str[1] = bfr.readLine();
+			return str;
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("in LocalConnector: Error: File not found Exception");
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("in LocalConnector: Error: unsupported encoding Exception");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("in LocalConnector: Error: IOException");
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 
 	
