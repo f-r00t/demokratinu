eb027d8be06899dee9666d9f3cb3cba97d19ebf6
HalfLeif <edarathaan@hotmail.com>
2013-04-26 15:12:11 +0200

    Removed Traff.java, using Article.java instead

--- demokratinu.src/src/com/example/pocketpolitics/model/Article.java
+++ demokratinu.src/src/com/example/pocketpolitics/model/Article.java
@@ -8,14 +8,52 @@ public class Article extends LikeableItem {
 	private String title;
 	private String dokid;
 	
+	private int traffnummer;
+	private String datum;
+	private String id;
+	private String titel;
+	private String rm;
+	private String relaterat_id;
+	private String beteckning;
+	private double score;
+	private String notisrubrik;
+	private String notis;
+	private String beslutsdag;
+	private int beslutad;
+	
 	public Article(String content, Date date, boolean isHidden, int opinion,
 			int nbrOfLikes, int nbrOfDislikes, List<Comment> replies, 
-			String title, String dokid) {
+			String title, String dokid,
+			int traffnummer,
+			String datum,
+			String id,
+			String titel,
+			String rm,
+			String relaterat_id,
+			String beteckning,
+			double score,
+			String notisrubrik,
+			String notis,
+			String beslutsdag,
+			int beslutad) {
 		
 		super(content, date, isHidden, opinion, nbrOfLikes, nbrOfDislikes, replies);
 		
 		this.title = title;
 		this.dokid = dokid;
+		
+		this.traffnummer = traffnummer;
+		this.datum = datum;
+		this.id = id;
+		this.titel = titel;
+		this.rm = rm;
+		this.relaterat_id =relaterat_id;
+		this.beteckning = beteckning;
+		this.score=score;
+		this.notisrubrik=notisrubrik;
+		this.notis=notis;
+		this.beslutsdag=beslutsdag;
+		this.beslutad=beslutad;
 	}
 	
 	// Temporary constructor
