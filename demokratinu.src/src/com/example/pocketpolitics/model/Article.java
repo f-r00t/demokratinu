93d57ca833051182463fe8cb14d98884cfbe65d5
HalfLeif <edarathaan@hotmail.com>
2013-04-26 15:15:48 +0200

    Removed duplicate fields

--- demokratinu.src/src/com/example/pocketpolitics/model/Article.java
+++ demokratinu.src/src/com/example/pocketpolitics/model/Article.java
@@ -4,9 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 public class Article extends LikeableItem {
-
-	private String title;
-	private String dokid;
 	
 	private int traffnummer;
 	private String datum;
@@ -23,7 +20,6 @@ public class Article extends LikeableItem {
 	
 	public Article(String content, Date date, boolean isHidden, int opinion,
 			int nbrOfLikes, int nbrOfDislikes, List<Comment> replies, 
-			String title, String dokid,
 			int traffnummer,
 			String datum,
 			String id,
@@ -38,9 +34,6 @@ public class Article extends LikeableItem {
 			int beslutad) {
 		
 		super(content, date, isHidden, opinion, nbrOfLikes, nbrOfDislikes, replies);
-		
-		this.title = title;
-		this.dokid = dokid;
 		
 		this.traffnummer = traffnummer;
 		this.datum = datum;
@@ -157,36 +150,4 @@ public class Article extends LikeableItem {
 		this.beslutad = beslutad;
 	}
 
-	
-
-
-	/**
-	 * @return The title (shorter description) of the article
-	 */
-	public String getTitle() {
-		return title;
-	}
-
-	/**
-	 * @param title The title (shorter description) of the article
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	/**
-	 * 
-	 * @return The document id of this article on the Riksdag database.
-	 */
-	public String getDokid() {
-		return dokid;
-	}
-	
-	/**
-	 * 
-	 * @param dokid The document id of this article on the Riksdag database.
-	 */
-	public void setDokid(String dokid) {
-		this.dokid = dokid;
-	}
 }
