91f2f85e86d43bd9db33334a08fca41c0e72cffb
HalfLeif <edarathaan@hotmail.com>
2013-04-26 14:20:18 +0200

    Added another filter option to QueryParam

--- demokratinu.src/src/com/example/pocketpolitics/net/QueryParam.java
+++ demokratinu.src/src/com/example/pocketpolitics/net/QueryParam.java
@@ -22,7 +22,9 @@ public class QueryParam {
 	 * @param dateFrom  Search for articles after this date, leave empty "" for no restriction
 	 * @param dateTo	Search for articles up to this date, leave empty "" for no restriction
 	 * @param page		Get result page, default 1
-	 * @param sort		Sort results: default = sort after date; 1 = sort after relevance. Relevance is determined by data.Riksdagen.se 
+	 * @param sort		Sort results: default = sort after date (only closed issues);
+	 * 0 = sort after date (all issues); 
+	 * 1 = sort after relevance (all issues). Relevance is determined by data.Riksdagen.se 
 	 */
 	QueryParam(String dateFrom, String dateTo, int page, int sort){
 		this.dateFrom = dateFrom;
@@ -37,8 +39,11 @@ public class QueryParam {
 		if(sort==1){
 			this.sort="rel";
 		}
-		else{
+		else if(sort==0){
 			this.sort="datum";
+		}
+		else{
+			this.sort="beslutsdag";
 			
 		}
 	}
