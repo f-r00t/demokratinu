83216bbf83d85399320f8ba1828ec6d22e29ee9d
HalfLeif <edarathaan@hotmail.com>
2013-04-30 12:10:56 +0200

    Added Enum Utskott for filtering

---
+++ demokratinu.src/src/com/example/pocketpolitics/model/Utskott.java
@@ -0,0 +1,32 @@
+package com.example.pocketpolitics.model;
+
+public enum Utskott {
+
+	Arbetsmarknad ("AU"),
+	Civil("CU"),
+	Finans("FiU"),
+	Foersvar("F%C3%B6U"),
+	Justitie("JuU"),
+	Konstitution("KU"),
+	Kultur("KrU"),
+	MiljoJordbruk("MJU"),
+	Naering("NU"),
+	Skatt("SkU"),
+	SocialFoersaekring("SfU"),
+	Social("SoU"),
+	Trafik("TU"),
+	Utbildning("UbU"),
+	Utrikes("UU");
+	
+	
+	private String org;
+	
+	Utskott(String org){
+		this.org = org;
+	}
+	
+	public String getQueryName(){
+		return org;
+	}
+	
+}
