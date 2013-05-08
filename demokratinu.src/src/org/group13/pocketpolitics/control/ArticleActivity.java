e63cf487faf451a0fb2ad820789f7d8155a5618b
Björn <theturnipman@gmail.com>
2013-05-08 14:34:48 +0200

    Added onCreate methods to MotionActivity and ArticleActivity.

--- demokratinu.src/src/org/group13/pocketpolitics/control/ArticleActivity.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/ArticleActivity.java
@@ -1,7 +1,11 @@
 package org.group13.pocketpolitics.control;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ArticleActivity extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
 
+		super.onCreate(savedInstanceState);
+	}
 }
--- demokratinu.src/src/org/group13/pocketpolitics/control/MotionActivity.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/MotionActivity.java
@@ -1,7 +1,12 @@
 package org.group13.pocketpolitics.control;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MotionActivity extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+	}
 
 }
