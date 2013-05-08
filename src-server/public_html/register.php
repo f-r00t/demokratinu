80620e68cfd274d42e3eccb8022c31a9757d8ca4
Johan Hasselqvist <u.j.hasselqvist@gmail.com>
2013-05-08 16:19:55 +0200

    Created a register.php-file, should be working, but doesnt check wether valid values were inputted

--- src-server/public_html/register.php
+++ src-server/public_html/register.php
@@ -1,26 +1,49 @@
 <?php
+
+// TODO: Check values for valid emails/usernames/passwords
+
 header("HTTP/1.0 404 Not Found"); // Makes this not show up in google searches etc
 
-if (isset($_GET['email']) && isset($_GET['pass']) {
+if (isset($_GET['email']) && isset($_GET['user']) && isset($_GET['pass']) {
 	
 	require_once("../includes/dbcx.php");
     $dbh = dbcx();
           
-    $username = trim($_GET['email']);
-    $pass = $_GET['pass'];
-            
-    $sql = "SELECT password FROM users WHERE email = :email";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(":email", $email);
+	$email = trim($_GET['email']);
+	$username = trim($_GET['user']);
+    $pass = $_GET['pass'];	  
+	
+	$sql = "SELECT username, email FROM users";
+	$stmt = $dbh->prepare($sql);
     $stmt->execute();
-    $dbcollected = $stmt->fetch();
-    
-    if ($dbcollected['password'] != crypt($pass, $dbcollected['password'])) {
-        echo "0"; // Wrong email/pass
-    }
-    else {
-	    echo "1"; // Success
-    }
+	
+	while($dbcollected = $stmt->fetch()) {
+        if ($dbcollected['username'] == $username) {
+			$status = "0";
+		}
+		if ($dbcollected['email'] == $email) {
+			$status = "0";
+		}
+	}
+	
+    if (status != "0") {
+		$salt = uniqid('', true);
+        $pass = crypt($pass, '$6$' . $salt . '$');
+		
+		$sql = "INSERT INTO users (username, email, password, date)
+		        VALUES (:username, :email, :password, NOW())";
+				
+		$stmt = $dbh->prepare($sql);
+		$stmt->bindParam(":username", $username);
+		$stmt->bindParam(":email", $email);
+		$stmt->bindParam(":password", $pass);
+		$stmt->execute();
+		
+		echo "1"; // Success
+	}
+	else {
+		echo "0"; // Either the username or email was already taken
+	}
 }
 else {
 	echo "Error"; // Should never happen
