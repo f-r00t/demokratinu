0cb0072294a777ef0752ba3832f5148f1454028e
Björn <theturnipman@gmail.com>
2013-05-10 12:25:26 +0200

    Added TextView variable in ArticleActivity.

--- demokratinu.src/src/org/group13/pocketpolitics/control/ArticleActivity.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/ArticleActivity.java
@@ -4,11 +4,15 @@ import org.group13.pocketpolitics.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ArticleActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_article);
+		
+		TextView title = (TextView)findViewById(R.id.activityArticleTitle);
+		title.setText("Bazinga!");
 	}
 }
