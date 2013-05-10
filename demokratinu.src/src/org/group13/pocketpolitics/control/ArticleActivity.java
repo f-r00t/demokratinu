d7e60ac970840ca12094e5c2c10e09e96430a276
Björn <theturnipman@gmail.com>
2013-05-10 13:11:14 +0200

    Added setTitle method to ArticleActivity.

--- demokratinu.src/src/org/group13/pocketpolitics/control/ArticleActivity.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/ArticleActivity.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class ArticleActivity extends Activity {
+	private TextView titleTextView;
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_article);
 		
-		TextView title = (TextView)findViewById(R.id.activityArticleTitle);
-		title.setText("Bazinga!");
+		titleTextView = (TextView)findViewById(R.id.activityArticleTitle);
+		titleTextView.setText("Bazinga!");
+	}
+
+	public void setTitle(String title){
+		titleTextView.setText(title);
 	}
 }
