25716f92379fc7aa6a71fe3b2e876790695fdf73
Björn <theturnipman@gmail.com>
2013-05-10 22:42:53 +0200

    Created constructor to MotionListAdapter.

--- demokratinu.src/src/org/group13/pocketpolitics/view/MotionListAdapter.java
+++ demokratinu.src/src/org/group13/pocketpolitics/view/MotionListAdapter.java
@@ -1,10 +1,29 @@
 package org.group13.pocketpolitics.view;
 
+import java.util.ArrayList;
+
+import org.group13.pocketpolitics.model.Article;
+import org.group13.pocketpolitics.model.Motion;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
-public class MotionListAdapter extends BaseAdapter{
+public class MotionListAdapter extends BaseAdapter {
+
+	LayoutInflater inflater;
+	ArrayList<Motion> items;
+
+	public MotionListAdapter(Activity context, ArrayList<Motion> items) {
+		super();
+
+		this.items = items;
+		this.inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
 
 	@Override
 	public int getCount() {
