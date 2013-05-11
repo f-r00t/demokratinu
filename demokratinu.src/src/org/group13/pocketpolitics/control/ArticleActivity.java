f93c2e88cc13fbd4da3c14aaee4237d167781c26
Björn <theturnipman@gmail.com>
2013-05-11 15:36:01 +0200

    ArticleActivity now implements ActivityNetInterface.

--- demokratinu.src/src/org/group13/pocketpolitics/control/ArticleActivity.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/ArticleActivity.java
@@ -1,12 +1,14 @@
 package org.group13.pocketpolitics.control;
 
 import org.group13.pocketpolitics.R;
+import org.group13.pocketpolitics.net.ActivityNetInterface;
+import org.group13.pocketpolitics.net.QueryResult;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class ArticleActivity extends Activity {
+public class ArticleActivity extends Activity implements ActivityNetInterface<QueryResult>{
 	private TextView titleTextView;
 	public void onCreate(Bundle savedInstanceState) {
 
@@ -19,5 +21,29 @@ public class ArticleActivity extends Activity {
 
 	public void setTitle(String title){
 		titleTextView.setText(title);
+	}
+
+	@Override
+	public void onPreExecute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProgressUpdate(Integer procent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSuccess(QueryResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFailure(String message) {
+		// TODO Auto-generated method stub
+		
 	}
 }
