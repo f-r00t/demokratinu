d1a733001fa56d56275a5cb9af4725bdbac65f27
Johan Hasselqvist <u.j.hasselqvist@gmail.com>
2013-05-13 21:34:45 +0200

    Fixing minor variable bug

--- src-server/public_html/register.php
+++ src-server/public_html/register.php
@@ -6,10 +6,34 @@
 
 if (isset($_GET['email']) && isset($_GET['user']) && isset($_GET['pass'])) {
 	
-	require_once("dbcx.php");
 	
-    //$dbh = dbcx();
-         
+	$settings = array(
+	'phptype'  => 'mysql',
+    'hostspec' => 'localhost',
+    'database' => '',
+    'username' => '',
+    'password' => ''
+	);
+
+	function dbcx() {
+		$dsnstr = "{$settings['phptype']}:host={$settings['hostspec']};dbname={$settings['database']}";
+		$dbuser = $settings['username'];
+		$dbpass = $settings['password'];
+		
+		$db = new PDO($dsnstr, $dbuser, $dbpass);
+		
+		$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+		$charset_sql = "SET NAMES 'UTF8' COLLATE 'utf8_swedish_ci'";
+		$db->query($charset_sql);
+
+		$dbtime = get_setting('dbtime');
+		$ts_sql = "SET time_zone = '$dbtime'";
+		$svar   = $db->query($ts_sql);
+	}
+
+
+	
 	$email = trim($_GET['email']);
 	$username = trim($_GET['user']);
     $pass = $_GET['pass'];	  
