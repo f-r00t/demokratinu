40e4fea3121b2f1216ee13be00aaa4bc714d4b4a
HalfLeif <edarathaan@hotmail.com>
2013-05-14 15:12:58 +0200

    Updated manifest tester implementation target package

--- demokratinu.src/src/org/group13/pocketpolitics/control/ArticleMemoryController.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/ArticleMemoryController.java
@@ -7,16 +7,29 @@ import org.group13.pocketpolitics.model.Article;
 import org.group13.pocketpolitics.net.Filter;
 import org.group13.pocketpolitics.net.QueryResult;
 
+import android.util.Log;
+
 public class ArticleMemoryController {
 	private static ArticleMemoryController INSTANCE;
 	
 	private List<Article> articles;
 	private Filter filter;
 	private int lastPage;
+	private int totalPages;
 	
 	public static int retrievedPage(QueryResult result){
 		checkInstance();
-		return ++INSTANCE.lastPage;
+		INSTANCE.articles.addAll(result.getArts());
+		if(result.getThisPage() != ++INSTANCE.lastPage){
+			Log.w(ArticleMemoryController.class.getSimpleName(), "PocketDebug: wrong papge count! internally: "+INSTANCE.lastPage+", riksdag count: "+result.getThisPage());
+			return -1;
+		}
+		return result.getThisPage();
+	}
+	
+	public static int totalPages(){
+		checkInstance();
+		return INSTANCE.totalPages;
 	}
 	
 	public static List<Article> articles(){
@@ -26,9 +39,13 @@ public class ArticleMemoryController {
 	
 	public static void newSearch(Filter f){
 		checkInstance();
+		if(f.equals(INSTANCE.filter)){
+			return;
+		}
 		INSTANCE.filter = f;
 		INSTANCE.articles.clear();
 		INSTANCE.lastPage = 0;
+		INSTANCE.totalPages = -1;
 	}
 	
 	public static Article article(int ix){
@@ -42,6 +59,7 @@ public class ArticleMemoryController {
 	private ArticleMemoryController(){
 		articles = new ArrayList<Article>();
 		lastPage=0;
+		totalPages = -1;
 		filter = null;
 	}
 	
