b503bbac0b621d5786654f87d105ff423f91dc64
Hasselmannen <johan_hasselqvist@hotmail.com>
2013-05-15 19:16:27 +0200

    minor adjustments for convenience

--- src-server/public_html/register.php
+++ src-server/public_html/register.php
@@ -2,13 +2,13 @@
 
 // TODO: Check values for valid emails/usernames/passwords
 
-//header("HTTP/1.0 404 Not Found"); // Makes this not show up in google searches etc
+$status = "1";
 
 if (isset($_GET['email']) && isset($_GET['user']) && isset($_GET['pass'])) {
-	
+
 	require_once("../../www-includes/dbcx.php");
-    $dbh = dbcx();
-	
+        $dbh = dbcx();
+
 	$email = trim($_GET['email']);
 	$username = trim($_GET['user']);
     $pass = $_GET['pass'];	  
@@ -16,8 +16,6 @@ if (isset($_GET['email']) && isset($_GET['user']) && isset($_GET['pass'])) {
 	$sql = "SELECT username, email FROM users";
 	$stmt = $dbh->prepare($sql);
     $stmt->execute();
-	
-	$status = "1";
 	
 	while($dbcollected = $stmt->fetch()) {
         if ($dbcollected['username'] == $username) {
@@ -40,14 +38,7 @@ if (isset($_GET['email']) && isset($_GET['user']) && isset($_GET['pass'])) {
 		$stmt->bindParam(":email", $email);
 		$stmt->bindParam(":password", $pass);
 		$stmt->execute();
-		
-		echo "1"; // Success
-	}
-	else {
-		echo "0"; // Either the username or email was already taken
 	}
 }
-else {
-	echo "Error"; // Should never happen
-}
+echo $status;
 ?>
