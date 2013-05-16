d144a528c2078b3f00d2d9063d31eee6ef49a7da
Johan Hasselqvist <u.j.hasselqvist@gmail.com>
2013-05-16 19:54:29 +0200

    added some functionality to the registrationActivity

--- demokratinu.src/src/org/group13/pocketpolitics/control/RegistrationActivity.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/RegistrationActivity.java
@@ -6,16 +6,26 @@ import org.group13.pocketpolitics.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 import android.support.v4.app.NavUtils;
+import android.text.TextUtils;
 
 public class RegistrationActivity extends Activity {
-	
+
 	// Registration values
 	private String username;
+	private String email;
 	private String password;
+	
+	// UI references
+	private EditText usernameView;
+	private EditText emailView;
+	private EditText passwordView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,14 @@ public class RegistrationActivity extends Activity {
 		setContentView(R.layout.activity_registration);
 		// Show the Up button in the action bar.
 		setupActionBar();
+
+		findViewById(R.id.buttonSendRegistration).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						attemptRegister();
+					}
+				});
 	}
 
 	/**
@@ -56,6 +74,70 @@ public class RegistrationActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void attemptRegister() {
+		// Reset errors.
+		emailView.setError(null);
+		passwordView.setError(null);
+		usernameView.setError(null);
+
+		// Store values at the time of the login attempt.
+		email = emailView.getText().toString().trim();
+		password = passwordView.getText().toString();
+		username = usernameView.getText().toString().trim();
+
+		boolean cancel = false;
+		View focusView = null;
+		
+		// Check for valid username.
+		if (TextUtils.isEmpty(username)) {
+			usernameView.setError(getString(R.string.error_field_required));
+			focusView = usernameView;
+			cancel = true;
+		}
+		else if (username.length() < 2) {
+			usernameView.setError(R.string.error_invalid_username);
+			focusView = usernameView;
+			cancel = true;
+		}
+		
+	    // Check for a valid password.
+		if (TextUtils.isEmpty(password)) {
+			passwordView.setError(getString(R.string.error_field_required));
+			focusView = passwordView;
+			cancel = true;
+		}
+		else if (password.length() < 4) {
+			passwordView.setError(getString(R.string.error_invalid_password));
+			focusView = passwordView;
+			cancel = true;
+		}
+		
+		// Check for a valid email address.
+		if (TextUtils.isEmpty(email)) {
+			emailView.setError(getString(R.string.error_field_required));
+			focusView = emailView;
+			cancel = true;
+		}
+		else if (email.length() < 3) {
+			emailView.setError(getString(R.string.error_invalid_email));
+			focusView = emailView;
+			cancel = true;
+		}
+		
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// if (Retriever.register()) {
+				Intent intent = new Intent(getApplicationContext(), FrontPageActivity.class);
+				startActivity(intent);
+				finish();
+			//}
+		}
+
 	}
 
 }
