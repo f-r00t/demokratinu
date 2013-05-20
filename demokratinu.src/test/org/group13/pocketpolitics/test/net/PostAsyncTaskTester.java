81273dffaef223f34f4a8bd9244825dc564c5923
HalfLeif <edarathaan@hotmail.com>
2013-05-20 11:21:57 +0200

    Init testing PostAsyncTask

--- demokratinu.src/test/org/group13/pocketpolitics/test/net/PostAsyncTaskTester.java
+++ demokratinu.src/test/org/group13/pocketpolitics/test/net/PostAsyncTaskTester.java
@@ -1,13 +1,37 @@
 package org.group13.pocketpolitics.test.net;
 
 import org.group13.pocketpolitics.model.user.Account;
+import org.group13.pocketpolitics.net.server.ServerInterface;
 import org.group13.pocketpolitics.net.server.Syncer;
 
 import android.test.AndroidTestCase;
+import android.util.Log;
 
-public class PostAsyncTaskTester extends AndroidTestCase {
+public class PostAsyncTaskTester extends AndroidTestCase implements ServerInterface{
 
+	private boolean finished;
+	
 	public void testRegister(){
 		Syncer.register(new Account("leif@testpolitics.se", "The HalfLeif untested", "12345"));
+		
+		this.finished=false;
+		
+		try {
+			while(!finished){
+				Thread.sleep(500);
+				Log.i(this.getClass().getSimpleName(), "PocketDebug: Thread waited another 500 ms");
+			}
+			
+		} catch (InterruptedException e) {
+			Log.e(this.getClass().getSimpleName(), "PocketDebug: Interruption error!");
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void messageReturned(String msg) {
+		this.finished=true;
+		Log.w(this.getClass().getSimpleName(), "PocketDebug: Message returned: "+msg);
 	}
 }
