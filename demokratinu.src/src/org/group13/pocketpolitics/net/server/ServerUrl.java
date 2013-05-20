c6e873fe65407cd0fb16d6fc6c4fde73186d4978
HalfLeif <edarathaan@hotmail.com>
2013-05-20 11:07:53 +0200

    Changed ServerUrl to package access

--- demokratinu.src/src/org/group13/pocketpolitics/net/server/ServerUrl.java
+++ demokratinu.src/src/org/group13/pocketpolitics/net/server/ServerUrl.java
@@ -1,6 +1,6 @@
 package org.group13.pocketpolitics.net.server;
 
-public enum ServerUrl {
+enum ServerUrl {
 	Register("http://fruktnet.no-ip.org/pocketpolitics/register.php"),
 	Authenticate("http://fruktnet.no-ip.org/pocketpolitics/authenticate.php");
 	
