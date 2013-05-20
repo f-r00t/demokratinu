49758b2544d2e6e7f6bb3465759856c1c22729dd
HalfLeif <edarathaan@hotmail.com>
2013-05-20 11:52:51 +0200

    Reading inputstream form HttpEntity

--- demokratinu.src/src/org/group13/pocketpolitics/net/server/PostAsyncTask.java
+++ demokratinu.src/src/org/group13/pocketpolitics/net/server/PostAsyncTask.java
@@ -1,7 +1,9 @@
 package org.group13.pocketpolitics.net.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,15 +43,31 @@ class PostAsyncTask extends AsyncTask<Void, Integer, HttpEntity> {
 	@Override
 	protected void onPostExecute(HttpEntity msg){
 		List<String> listr = new ArrayList<String>();
+		BufferedReader brd=null;
 		try {
 			InputStream instr = msg.getContent();
+			brd = new BufferedReader(new InputStreamReader(instr));
+			
+			String line;
+			while((line = brd.readLine())!=null){
+				listr.add(line);
+			}
 			
 		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
+			Log.w(this.getClass().getSimpleName(), "PocketDebug: in onPostExecute(): Illegal state exception"+e);
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			Log.w(this.getClass().getSimpleName(), "PocketDebug: in onPostExecute(): IOexception"+e);
 			e.printStackTrace();
+		} finally {
+			if(brd!=null){
+				try {
+					brd.close();
+				} catch (IOException e) {
+					Log.w(this.getClass().getSimpleName(), "PocketDebug: in onPostExecute(): Failed at closing reader! Possible resource leak");
+					e.printStackTrace();
+				}
+			}
 		}
 		act.messageReturned(listr);
 	}
