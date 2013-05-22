ce0b14e807115ba0fdca582dc3767e1550dba94f
HalfLeif <edarathaan@hotmail.com>
2013-05-22 15:34:54 +0200

    Added new operations in ServerOperation

--- demokratinu.src/src/org/group13/pocketpolitics/net/server/ServerOperation.java
+++ demokratinu.src/src/org/group13/pocketpolitics/net/server/ServerOperation.java
@@ -2,7 +2,10 @@ package org.group13.pocketpolitics.net.server;
 
 enum ServerOperation {
 	Register("http://fruktnet.no-ip.org/pocketpolitics/register.php"),
-	Authenticate("http://fruktnet.no-ip.org/pocketpolitics/authenticate.php");
+	Authenticate("http://fruktnet.no-ip.org/pocketpolitics/authenticate.php"),
+	PostOpinion("http://fruktnet.no-ip.org/pocketpolitics/post-opinion.php"),
+	PostComment("http://fruktnet.no-ip.org/pocketpolitics/post-comment.php"),
+	GetArticleData("http://fruktnet.no-ip.org/pocketpolitics/get-article-data.php");
 	
 	private String url;
 	ServerOperation(String url){
