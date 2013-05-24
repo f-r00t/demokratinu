c1ee81de36c2efafe8c729c75f7baebe7d8c80f7
HalfLeif <edarathaan@hotmail.com>
2013-05-24 20:01:09 +0200

    Cleaning

--- demokratinu.src/src/org/group13/pocketpolitics/control/FrontPageActivity.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/FrontPageActivity.java
@@ -23,7 +23,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
