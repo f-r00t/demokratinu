964073729ebaf4cc716f21bc98996eb689792e1c
Björn <theturnipman@gmail.com>
2013-05-24 21:54:03 +0200

    CommitteeProposalListAdapter now displays a list of committee_proposal_list_items

--- demokratinu.src/src/org/group13/pocketpolitics/view/CommitteeProposalListAdapter.java
+++ demokratinu.src/src/org/group13/pocketpolitics/view/CommitteeProposalListAdapter.java
@@ -3,6 +3,7 @@ package org.group13.pocketpolitics.view;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.Inflater;
 
 import org.group13.pocketpolitics.R;
 import org.group13.pocketpolitics.model.riksdag.CommitteeProposal;
@@ -21,6 +22,7 @@ import android.widget.TextView;
 public class CommitteeProposalListAdapter extends BaseExpandableListAdapter {
 
 	private Context context;
+	private LayoutInflater inflater;
 	private List<String> listTopics;
 	private List<List<CommitteeProposal>> listNotes;
 
@@ -29,6 +31,8 @@ public class CommitteeProposalListAdapter extends BaseExpandableListAdapter {
 		super();
 
 		this.context = context;
+		inflater = (LayoutInflater) context.
+				getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		
 		listTopics = new ArrayList<String>(items.size());
 		listNotes = new ArrayList<List<CommitteeProposal>>();
@@ -59,6 +63,34 @@ public class CommitteeProposalListAdapter extends BaseExpandableListAdapter {
 	@Override
 	public View getChildView(int groupPosition, int childPosition,
 			boolean isLastChild, View convertView, ViewGroup parent) {
+		
+		View vi = convertView;
+
+
+		if (convertView == null) {
+			vi = inflater.inflate(R.layout.committee_proposal_list_item, null);
+		}
+
+		// Sets the title text
+		
+		//Set text
+		TextView committeProposalText = (TextView) vi.findViewById(R.id.committeeProposalListItemText);
+		
+		// Assign the note text
+		committeProposalText.setText(getChild(groupPosition, childPosition).getForslag());
+
+		// Make the note italic
+		committeProposalText.setTypeface(Typeface.DEFAULT, Typeface.ITALIC);
+
+		// indent the child element a bit
+		committeProposalText.setPadding(20, 0, 0, 0);
+		
+		return vi;
+		
+		
+		
+		
+		/*
 		TextView row = (TextView) convertView;
 
 		if (row == null) {
@@ -77,6 +109,7 @@ public class CommitteeProposalListAdapter extends BaseExpandableListAdapter {
 		
 		//Log.i("Viking","In child: "+getChild(groupPosition, childPosition).getForslag());
 		return row;
+		*/
 	}
 
 	@Override
@@ -134,98 +167,6 @@ public class CommitteeProposalListAdapter extends BaseExpandableListAdapter {
 
 }	
 	
-/*	private LayoutInflater inflater;
-	private List<CommitteeProposal> items;
-	private Context context;
-	private List<String> parentList;
-	private List<List<CommitteeProposal>> childList;
-	
-	public CommitteeProposalListAdapter(Context context,
-			List<CommitteeProposal> items) {
-		super();
-		
-		parentList = new ArrayList<String>();
-		childList = new ArrayList<List<CommitteeProposal>>();
-		
-		//This might not be needed and should probably be removed.
-		this.items = items;
-		Log.i("Viking","childList");
-		childList.add(this.items);
-		Log.i("Viking","after childList");
-		
-		for (int i=0;i<items.size();i++){
-			Log.i("Viking","for "+i);
-			parentList.add(items.get(i).getTitle());
-		}
-		
-		//Not sure if it is a good idea to both have an inflater and a context
-		this.context = context;
-		this.inflater = (LayoutInflater) context.
-				getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		Log.i("Viking","In parent: "+parentList.size());
-		Log.i("Viking","In child: "+childList.get(0).size());
-	}
-	@Override
-	public Object getChild(int groupPosition, int childPosition) {
-		return (childList.get(groupPosition)).get(childPosition);
-	}
-
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		return childPosition;
-	}
-
-	@Override
-	public View getChildView(int groupPosition, int childPosition,
-			boolean isLastChild, View convertView, ViewGroup parent) {
-		Log.i("Viking","Child text: "+((CommitteeProposal)this.getChild(groupPosition, childPosition)).getForslag());
-		TextView tv = new TextView(context);
-		tv.setText(((CommitteeProposal)this.getChild(groupPosition, childPosition)).getForslag());
-		return tv;
-	}
-
-	@Override
-	public int getChildrenCount(int groupPosition) {
-		Log.i("Viking","In getChildrenCount "+(childList.get(groupPosition)).size());
-		return (childList.get(groupPosition)).size();
-	}
-
-	@Override
-	public Object getGroup(int groupPosition) {
-		return groupPosition; //parentList.get(groupPosition);
-	}
-
-	@Override
-	public int getGroupCount() {
-		return parentList.size();
-	}
-
-	@Override
-	public long getGroupId(int groupPosition) {
-		return groupPosition;
-	}
-
-	@Override
-	public View getGroupView(int groupPosition, boolean isExpanded,
-			View convertView, ViewGroup parent) {
-		TextView tv = new TextView(context);
-		tv.setText(parentList.get(groupPosition));
-		Log.i("Viking","Parent text: "+parentList.get(groupPosition)+" : "+isExpanded);
-		return tv;
-	}
-
-	@Override
-	public boolean hasStableIds() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		return true;
-	}
-
-}
 
 /*
  * 	LayoutInflater inflater;
