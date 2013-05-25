db86a20ef30c2428c2ece0815f4f10924a7ad246
HalfLeif <edarathaan@hotmail.com>
2013-05-25 09:46:28 +0200

    Refactored name to MoprositionActivity

---
+++ demokratinu.src/src/org/group13/pocketpolitics/control/MoprositionActivity.java
@@ -0,0 +1,14 @@
+package org.group13.pocketpolitics.control;
+
+import org.group13.pocketpolitics.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoprositionActivity extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+	}
+
+}
