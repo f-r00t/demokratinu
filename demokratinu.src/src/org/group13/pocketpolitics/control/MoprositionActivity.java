4f38c943f67f488e4e7ea7b35fa86b37c2d4a89a
Johan Hasselqvist <u.j.hasselqvist@gmail.com>
2013-05-25 15:38:38 +0200

    up-button now works (though not implemented as usually, this is kind of a workaround since there is more than 1 possible parentActivity, and we dont pass it to the MoprositionActivity)

--- demokratinu.src/src/org/group13/pocketpolitics/control/MoprositionActivity.java
+++ demokratinu.src/src/org/group13/pocketpolitics/control/MoprositionActivity.java
@@ -8,7 +8,9 @@ import org.group13.pocketpolitics.net.riksdag.Retriever;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 
@@ -29,6 +31,8 @@ public class MoprositionActivity extends Activity implements ActivityNetInterfac
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_moprosition);
 		
+		setupActionBar();
+		
 		this.progressBar = findViewById(R.id.progressBarContainer);
 		this.webView = (WebView) findViewById(R.id.moprosition_web_view);
 		
@@ -43,6 +47,32 @@ public class MoprositionActivity extends Activity implements ActivityNetInterfac
 		}
 		
 		orderMoprosition();
+	}
+	
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 	
 	private static boolean isMotion(String id){
